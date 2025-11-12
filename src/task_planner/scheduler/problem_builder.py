b533dc21640ff3f3e74a7825604e7903df2dee28
david <david.larosa@plug-in.it>
2025-11-12 17:52:59 +0100

    add job release, deadline, and due date handling; refactor task creation logic

--- src/task_planner/scheduler/problem_builder.py
+++ src/task_planner/scheduler/problem_builder.py
@@ -5,7 +5,8 @@ Problem builder for converting database models to PyJobShop problems.
 from datetime import datetime
 from typing import Dict, List, Optional
 
-from pyjobshop import Model
+from pyjobshop import MAX_VALUE, Model
+from pyjobshop import Task as PJTask
 
 from ..models import Person, Resource, Task
 
@@ -15,7 +16,7 @@ class ProblemBuilder:
 
     def __init__(self) -> None:
         self.model: Optional[Model] = None
-        self.task_mapping: Dict[int, int] = {}  # DB task id -> PyJobShop task index
+        self.task_mapping: Dict[int, PJTask] = {}  # DB task id -> PyJobShop task
         self.person_mapping: Dict[int, int] = {}  # DB person id -> PyJobShop machine index
         self.resource_mapping: Dict[int, int] = {}  # DB resource id -> PyJobShop resource index
 
@@ -71,55 +72,63 @@ class ProblemBuilder:
         # Build jobs
         for team_id, team_tasks in job_groups.items():
             job_name = f"team_{team_id}" if team_id else "no_team"
-            job = self.model.add_job(name=job_name)
+
+            # Aggregate dates
+            job_release = 0
+            job_deadline = MAX_VALUE
+            job_due = None
 
             for task in team_tasks:
-                # Convert duration from hours to time units (minutes)
+                if task.earliest_start:
+                    release_minutes = int((task.earliest_start - start_date).total_seconds() / 60)
+                    job_release = min(job_release or release_minutes, release_minutes)
+
+                if task.deadline:
+                    deadline_minutes = int((task.deadline - start_date).total_seconds() / 60)
+                    job_deadline = max(job_deadline or deadline_minutes, deadline_minutes)
+
+                if task.due_date:
+                    due_minutes = int((task.due_date - start_date).total_seconds() / 60)
+                    job_due = max(job_due or due_minutes, due_minutes)
+
+            job = self.model.add_job(
+                name=job_name,
+                release_date=job_release,
+                deadline=job_deadline,
+                due_date=job_due,
+            )
+
+            for task in team_tasks:
                 duration = int(task.duration * 60)
 
-                # Determine which machines can perform this task
-                # If task has required skills, filter people by skills
-                # For simplicity, we'll allow any available person for now
-                eligible_machines = list(self.person_mapping.values())
+                earliest_start = (
+                    int((task.earliest_start - start_date).total_seconds() / 60)
+                    if task.earliest_start
+                    else 0
+                )
+                latest_end = (
+                    int((task.deadline - start_date).total_seconds() / 60)
+                    if task.deadline
+                    else MAX_VALUE
+                )
 
-                if eligible_machines:
-                    # Add task with one or more processing modes
-                    pj_task = job.add_task(
-                        name=f"task_{task.id}",
-                        duration=duration,
-                        machines=[eligible_machines[0]],  # Simplified: use first eligible machine
-                    )
+                pj_task = self.model.add_task(
+                    job=job,
+                    earliest_start=earliest_start,
+                    latest_end=earliest_start + duration if latest_end == MAX_VALUE else latest_end,
+                    name=f"task_{task.id}",
+                )
 
-                    self.task_mapping[task.id] = len(self.model.tasks()) - 1
-
-                    # Add release date if specified
-                    if task.earliest_start:
-                        release_minutes = int(
-                            (task.earliest_start - start_date).total_seconds() / 60
-                        )
-                        if release_minutes > 0:
-                            pj_task.add_release_date(release_minutes)
-
-                    # Add deadline if specified
-                    if task.deadline:
-                        deadline_minutes = int((task.deadline - start_date).total_seconds() / 60)
-                        if deadline_minutes > 0:
-                            pj_task.add_deadline(deadline_minutes)
-
-                    # Add due date if specified
-                    if task.due_date:
-                        due_minutes = int((task.due_date - start_date).total_seconds() / 60)
-                        if due_minutes > 0:
-                            pj_task.add_due_date(due_minutes)
+                self.task_mapping[task.id] = pj_task
 
         # Add precedence constraints
         for task in tasks:
             if task.predecessor_id and task.id in self.task_mapping:
                 if task.predecessor_id in self.task_mapping:
-                    pred_idx = self.task_mapping[task.predecessor_id]
-                    curr_idx = self.task_mapping[task.id]
+                    pred_task = self.task_mapping[task.predecessor_id]
+                    curr_task = self.task_mapping[task.id]
                     # Add precedence constraint: predecessor must finish before current starts
-                    self.model.add_precedence(pred_idx, curr_idx)
+                    self.model.add_consecutive(pred_task, curr_task)
 
         return self.model
 
